7b5d5ebc5485004ae3f196a69f63035f09dd9551
Sarah <sarahtnamikata@gmail.com>
2025-03-02 17:22:39 -0300

    Changing from NgModule to Standalone components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from '../components/login/login.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  standalone: false,
+  standalone: true,
+  imports: [
+    CommonModule, 
+    LoginComponent 
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
